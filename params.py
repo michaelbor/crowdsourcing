5c157b837c42983193b007f17ecd506f6a90fa70
Michael Borokhovich <michaelbor@gmail.com>
2015-07-17 16:44:59 -0500

    params

--- params.py
+++ params.py
@@ -3,15 +3,16 @@ time_step = 600
 max_num_of_skills_steps = 3
 max_num_of_skills_worker = 3
 num_of_existing_skills = 5
-max_skill_time = 6000
-min_skill_time = 500
-#avail_time_avg = 5000
+max_skill_time = 600
+min_skill_time = 60
 arr_time_avg_gap = 10
 max_prio = 20
 num_of_steps_in_db = 344
 num_of_workers_in_db = 2500
-working_hours = [[8,16],[8,12],[12,16],[16,20],[18,20]]
-timezones = range(-12,13)
+#working_hours = [[8,16],[8,12],[12,16],[16,20],[18,20]]
+working_hours = [[9,17]]
+#timezones = range(-12,13)
+timezones = [-4,0,3,5]
 max_ordinal = 3
 num_of_new_tasks_per_hour = 100
 max_task_turnaround_days = 365
@@ -23,10 +24,9 @@ bins_num_of_bins = 1000000
 #bins_num_of_bins = 1000
 
 title_font = {'fontname':'Arial', 'size':'12', 'color':'black', 'weight':'normal',
-# title_font = {'fontname':'Helvetica', 'size':'18', 'color':'black', 'weight':'normal',
               'verticalalignment':'bottom'} # Bottom vertical alignment for more space
 axis_font = {'fontname':'Arial', 'size':'12','weight':'bold'}
 axis_font_x = {'fontname':'Arial', 'size':'10','weight':'bold'}
 
-ticksize = 15
+#ticksize = 15
 
